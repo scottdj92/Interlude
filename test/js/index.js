6aa297b7f88a7cf2c5713d55179581e61cb41fb9
dxn7335 <dxn7335@rit.edu>
2015-03-08 19:07:14 -0400

    Server identifies different sockets
    Testing with server sending messages to specific sockets.
    The coding practice may not be ideal but its something to start off on.
    (To be continued)

--- test/js/index.js
+++ test/js/index.js
@@ -3,34 +3,55 @@ var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
 var MobileDetect = require('mobile-detect');
+var users = [];
 
 app.get('/', function(req, res){
 	//the html string being sent
 
 	var md = new MobileDetect(req.headers['user-agent']);
 	var filepath = path.resolve(__dirname + '/../index.html');
-	if(md.mobile() !== null) {
-		filepath = path.resolve();
+	if(md.mobile() != null) {
+		filepath = path.resolve(__dirname + '/../mobile_index.html');
 	}
 	res.sendFile(filepath);
 });
 
+
+// **** ACTIONS DONE BY SERVER HAVE HIGHER PRIORITY AND INIT BEFORE CLIENT ACTIONS
+
 // Setup socket.io
 // listen to connection
 io.on('connection', function(socket){
-	console.log('a user connected');
+	//broadcast that a user has connected
+	//pass an object containing user informatiojn?
+    /*socket.broadcast.emit('Broadcast');
+	*/
+	var userId = socket.handshake.query.user;
+	console.log(userId +' connected.');
+  socket.join(userId);
+
+	var msg = { text:"Hello " + userId, id:"Admin"};
+	io.to(userId).emit('chat message', msg);
+	io.emit("chat message", msg);
+
 	// handle disconnects
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
 	});
 });
 
+
+
 // get sent message
 io.on('connection', function(socket){
+
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    console.log('message: ' + msg.text);
+
   });
 });
+
+
 
 // broadcast
 io.on('connection', function(socket){
