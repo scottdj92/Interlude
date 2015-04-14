4af3de3a76eb4423153b5a3a288b52c94589c189
Peter Gyory <ptg3662@rit.edu>
2015-04-14 12:07:44 -0400

    Canvas now locks to 16:9 ratio

--- test/js/game/draw.js
+++ test/js/game/draw.js
@@ -1,7 +1,16 @@
 //Contains draw methods for different game states, and helper functions
 "use strict";
 var game = game || {};
+//ratio 16 x 9
 
 game.draw = {
+  baseRatio: {x: 1600, y:900}, //What values will be determined with
+  currentRatio: {x: 0, y: 0}, //
   
+  setRatio : function(x, y) {
+
+  },
+  circle : function(ctx, x, y, r, color) {
+
+  }
 }
--- test/js/game/game.js
+++ test/js/game/game.js
@@ -153,6 +153,8 @@ game.interlude = {
   renderGame : function() {
     var self = this;//Save a reference to this
     this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);//clear the canvas
+    //this.fillStyle = "#112255";
+    //this.ctx.fillRect(0,0, this.canvas.width, this.canvas.height);//clear the canvas
     //loop through bubbles
     this.bubbles.forEach(function(bubble) {
       bubble.render(self.ctx);//draw each bubble
