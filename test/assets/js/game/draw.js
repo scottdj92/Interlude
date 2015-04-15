4ee6c624a079c8c33647519c7ec40dec82cacd81
Peter Gyory <ptg3662@rit.edu>
2015-04-15 10:06:57 -0400

    Added circle function to draw.js
    Also included draw in index.html

--- test/assets/js/game/draw.js
+++ test/assets/js/game/draw.js
@@ -1,16 +1,28 @@
 //Contains draw methods for different game states, and helper functions
 "use strict";
 var game = game || {};
-//ratio 16 x 9
-
+//Any values used as coordinates for functions here should be between 0 and 16/9
+//To keep everything relative to the canvas size
+//All values will be multiplied by canvas.height
+//This means that the corner of the screen will be (16/9 , 1)
 game.draw = {
-  baseRatio: {x: 1600, y:900}, //What values will be determined with
-  currentRatio: {x: 0, y: 0}, //
-  
-  setRatio : function(x, y) {
-
+  canvas : undefined,
+  ctx : undefined,
+  init : function(canvas, ctx) {
+    this.canvas = canvas;
+    this.ctx = ctx;
   },
-  circle : function(ctx, x, y, r, color) {
+  circle : function(x, y, r, color) {
+    var Xscaled = x * this.canvas.height;
+    var Yscaled = y * this.canvas.height;
+    var Rscaled = r * this.canvas.height;
 
+    this.ctx.save();//save the draw state
+    this.ctx.fillStyle = color;
+    this.ctx.beginPath();
+    this.ctx.arc(Xscaled, Yscaled, Rscaled, 0, Math.PI * 2);
+    this.ctx.closePath();
+    this.ctx.fill();
+    this.ctx.restore();//restore the draw state
   }
 }
